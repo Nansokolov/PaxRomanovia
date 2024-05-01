1a2c5be3256aa2d8459c6d05273e34b73d183eb6
Vlad Phokhnitskyi <66656318+Nansokolov@users.noreply.github.com>
2024-05-01 08:54:22 -0400

    Update cards.js

--- src/assets/cards.js
+++ src/assets/cards.js
@@ -26,6 +26,18 @@ const cards = [
       "text text text .... blablabla 1text text text .... blablabla 1text text text .... blablabla 1text text text .... blablabla 1text text text .... blablabla 1text text text .... blablabla 1text text text .... blablabla 1text text text .... blablabla 1text text text .... blablabla 1text text text .... blablabla 1text text text .... blablabla 1text text text .... blablabla 1text text text .... blablabla 1text text text .... blablabla 1text text text .... blablabla 1text text text .... blablabla 1text text text .... blablabla 1text text text .... blablabla 1",
   },
   {
+    place: [277, 569],
+    id: 6,
+    type: "strengthen",
+    region: "north",
+    year: "1640",
+    name: "New reveal",
+    period: "1642г.",
+    img: "pospolit",
+    description:
+      "Азовское осадное сидение донских казаков 1641 года является самым значительным событием русско- турецких отношений I половины XVII века.",
+  },
+  {
     place: [277, 519],
     id: 5,
     type: "strengthen",
@@ -34,30 +46,114 @@ const cards = [
     name: "New reveal",
     period: "1700-1800г.",
     img: "kazakrelocation",
-    description: "text text text .... blablabla 2",
+    description: "New reveal",
   },
   {
     place: [300, 300],
     id: 3,
     type: "experiments",
     region: "northernBlackSea",
-    year: "1620",
-    name: "try to destroy your ass",
-    period: "1800-1900г.",
+    year: "1660",
+    name: "Русско-польская война",
+    period: "1654–1667г.",
     img: "persidskiy",
-    description: "text text text .... blablabla 3",
+    description: "Русско-польская война",
   },
   {
     place: [793, 342],
     id: 4,
     type: "subjects",
     region: "farEast",
-    year: "1600",
-    name: "try to destroy your ass",
-    period: "1800-1900г.",
+    year: "1680",
+    name: "Чигиринские походы",
+    period: "1677–1679г.",
     img: "pospolit",
-    description: "text text text .... blablabla 3",
+    description: "Чигиринские походы",
   },
+  {
+    place: [423, 538],
+    id: 7,
+    //filters:
+    type: "conflict",
+    region: "uralSiberia",
+    year: "1680",
+    //data:
+    name: "Вечный мир с Речью Посполитой",
+    period: "1686г.",
+    img: "ekb",
+    description:
+      "Вечный мир с Речью Посполитой",
+  },
+  {
+    place: [277, 569],
+    id: 8,
+    //filters:
+    type: "conflict",
+    region: "uralSiberia",
+    year: "1700",
+    //data:
+    name: "Азовские походы Петра I",
+    period: "1695–1696г.",
+    img: "ekb",
+    description:
+      "Азовские походы Петра I",
+  },
+  {
+    place: [423, 238],
+    id: 9,
+    //filters:
+    type: "conflict",
+    region: "uralSiberia",
+    year: "1650",
+    //data:
+    name: "Соборное уложение",
+    period: "1649г.",
+    img: "ekb",
+    description:
+      "Соборное уложение — главный законодательный документ на ближайшие 180 лет",
+  },
+  {
+    place: [653, 338],
+    id: 10,
+    //filters:
+    type: "conflict",
+    region: "uralSiberia",
+    year: "1650",
+    //data:
+    name: "Соляной бунт",
+    period: "1648–1649г.",
+    img: "ekb",
+    description:
+      "Соляной бунт",
+  },
+  {
+    place: [623, 438],
+    id: 11,
+    //filters:
+    type: "conflict",
+    region: "uralSiberia",
+    year: "1660",
+    //data:
+    name: "Медный бунт",
+    period: "1662г.",
+    img: "ekb",
+    description:
+      "Медный бунт",
+  },
+  {
+    place: [323, 538],
+    id: 12,
+    //filters:
+    type: "conflict",
+    region: "uralSiberia",
+    year: "1670",
+    //data:
+    name: "Восстание Степана Разина",
+    period: "1670–1671г.",
+    img: "ekb",
+    description:
+      "Восстание Степана Разина",
+  }
 ];
 
 export default cards;
